3e96ce0fb9424a9138c5dd6de127a2d82770665d
TommyP <tommypizarro6@gmail.com>
2025-11-23 17:42:21 +0000

    Fix entrypoint.sh email and commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+set -e
 
 echo "=============="
 
 git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${INPUT_EMAIL}"
+git config --global user.email "${INPUT_EMAIL:-${GITHUB_ACTOR}@users.noreply.github.com}"
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
-git add -A && git commit -m "Update Feed" 
+git add -A
+git commit -m "Update Feed" || echo "No changes to commit"
 git push --set-upstream origin main
 
-echo "===============" 
+echo "==============="
